212047c3d08bbaba8b1385000e302ec906d8508e
Вячеслав Поморцев <xaoasakura1991@gmail.com>
2023-09-19 20:58:21 +0300

    change tests ng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void CashbackHackServiceTopUp900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void CashbackHackServiceTopUp1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void CashbackHackServiceTopUp0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+}
